1de5defcf49501479240d0a64c04b6c885388d6c
Sanket Waghmare <sawaghmare23@gmail.com>
2021-04-21 06:14:27 +0530

    Updated

--- LPRoute.py
+++ LPRoute.py
@@ -1,69 +1,107 @@
 import matplotlib.pyplot as plt
+import networkx as nx
 import pandas as pd
 import numpy as np
 from pulp import *
+import seaborn as sn
 import haversine as hs
+from haversine import Unit
+
+G = nx.random_geometric_graph(10, 0.3)
+pos = nx.get_node_attributes(G, "pos")
+dmin = 1
+ncenter = 0
+for n in pos:
+    x, y = pos[n]
+    d = (x - 0.5) ** 2 + (y - 0.5) ** 2
+    if d < dmin:
+        ncenter = n
+        dmin = d
+p = dict(nx.single_source_shortest_path_length(G, ncenter))
+plt.figure(figsize=(8, 8))
+nx.draw_networkx_edges(G, pos, nodelist=[ncenter], alpha=0.4)
+nx.draw_networkx_nodes(
+    G,
+    pos,
+    nodelist=list(p.keys()),
+    node_size=80,
+    node_color=list(p.values()),
+    cmap=plt.cm.Reds_r,
+)
+plt.xlim(-0.05, 1.05)
+plt.ylim(-0.05, 1.05)
+plt.axis("off")
+plt.show()
+
+ll = list()
+for i in pos:
+    ll.append(pos[i][0])
+rr = list()
+for i in pos:
+    rr.append(pos[i][1])
+    
+df = pd.DataFrame()
+df['Left'] = ll
+df['Right'] = rr
+df['coordinate'] = list(zip(df['Left'],df['Right']))
+df['ID'] = df.index
 
 
-df=pd.read_csv('../../../Datasets/cop_data.csv')
-df['coordinate'] = list(zip(df['Latitude'],df['Longitude']))
-
-
-def dist_from(loc1,loc2):
+def dist_from(loc1,loc2): 
     dist=hs.haversine(loc1,loc2)
     return round(dist,2)
-
-# parse location one by one to dist_from
+    
 for _,row in df.iterrows():
     df[row.ID]=df['coordinate'].apply(lambda x: dist_from(row.coordinate,x))
-
-distances_df=df.iloc[:,19:4183]
+    
+distances_df=df.iloc[:,4:4183]
 #distances_df.index=df.ID
 distances_df.insert(0, 'ID', df.ID)
 
+dist_dict={}
 
-#positions to be plotted
-locations = dict( ( ID, (df[['ID','Latitude','Longitude']].loc[ID, 'Longitude'], df[['ID','Latitude','Longitude']].loc[ID, 'Latitude']) ) for ID in df.index)
+locations = dict( ( ID, (df.loc[ID, 'Left'], df.loc[ID, 'Right']) ) for ID in df.index)
+locations
 
 for l in locations:
     lo = locations[l]
-    plt.plot(lo[0], lo[1], 'o')
-    plt.text(lo[0] + .01, lo[1], l, horizontalalignment='center', verticalalignment='center')
+    plt.plot(lo[0],lo[1],'o')
+    plt.text(lo[0]+.01,lo[1],l,horizontalalignment='center',verticalalignment='center')    
+plt.gca().axis('off');    
 
-plt.gca().axis('off');
+df1 = pd.DataFrame()
+df1 = df
+df1 = df1.drop(['Left'],axis=1)
+df1 = df1.drop(['Right'],axis=1)
+df1 = df1.drop(['coordinate'],axis=1)
+df1 = df1.drop(['ID'],axis=1)
+distance = df1
+distances = dict( ((l1,l2), distance.iloc[l1, l2] ) for l1 in locations for l2 in locations if l1!=l2)
 
-# get distance in a dictionary form
-distances = dict( ((l1,l2), distances_df.iloc[l1, l2] ) for l1 in locations for l2 in locations if l1!=l2)
 
-V = 3 #the number vehicles/people deployed
-
-#problem
+V = 3
 prob=LpProblem("vehicle", LpMinimize)
-#indicates if location i is connected to location j along route
 indicator = LpVariable.dicts('indicator',distances, 0,1,LpBinary)
-#elimiate subtours
 eliminator = LpVariable.dicts('eliminator', df.ID, 0, len(df.ID)-1, LpInteger)
+cost = lpSum([indicator[(i,j)]*distances[(i,j)] for (i,j) in distances])
+prob+=cost
 
-# constraints
+
 for v in df.ID:
-    cap = 1 if v != '9aed9ae5ee7f83638ee33ccff49cf6b9' else V
-    # inward possible route
-    prob += lpSum([indicator[(i, v)] for i in df.ID if (i, v) in indicator]) == cap
-    # outward possible route
-    prob += lpSum([indicator[(v, i)] for i in df.ID if (v, i) in indicator]) == cap
-
-# subtour elimination
-num = len(df.ID) / V
+    cap = 1 if v != 7 else V
+    #inward possible route
+    prob+= lpSum([ indicator[(i,v)] for i in df.ID if (i,v) in indicator]) ==cap
+    #outward possible route
+    prob+=lpSum([ indicator[(v,i)] for i in df.ID if (v,i) in indicator]) ==cap
+    
+num=len(df.ID)/V
 for i in df.ID:
     for j in df.ID:
-        if i != j and (i != '9aed9ae5ee7f83638ee33ccff49cf6b9' and j != '9aed9ae5ee7f83638ee33ccff49cf6b9') and (
-        i, j) in indicator:
-            prob += eliminator[i] - eliminator[j] <= (num) * (1 - indicator[(i, j)]) - 1
-
-
-
-%time prob.solve()
-print(LpStatus[prob.status])
+        if i != j and (i != 7 and j!= 7) and (i,j) in indicator:
+            prob += eliminator[i] - eliminator[j] <= (num)*(1-indicator[(i,j)]) - 1
+            
+            
+prob.solve()
 
 feasible_edges = [ e for e in indicator if value(indicator[e]) != 0 ]
 
@@ -73,30 +111,26 @@ def get_next_loc(initial):
     for e in edges:
         feasible_edges.remove(e)
     return edges
-
-routes = get_next_loc('9aed9ae5ee7f83638ee33ccff49cf6b9')
+    
+routes = get_next_loc(7)
 routes = [ [e] for e in routes ]
 
 for r in routes:
-    while r[-1][1] !='9aed9ae5ee7f83638ee33ccff49cf6b9':
+    while r[-1][1] !=7:
         r.append(get_next_loc(r[-1][1])[-1])
-
-for r in routes:
-    print(' -> '.join([ a for a,b in r]+['9aed9ae5ee7f83638ee33ccff49cf6b9']))
-
-#outline the routes
+        
 coloured_loc = [np.random.rand(3) for i in range(len(routes))]
 for r,co in zip(routes,coloured_loc):
     for a,b in r:
         l1,l2 = locations[a], locations[b]
-        plt.plot([l1[0],l2[0]],[l1[1],l2[1]], coloured_loc=co)
-
+        plt.plot([l1[0],l2[0]],[l1[1],l2[1]], color=co)
 for l in locations:
     lo = locations[l]
-    plt.plot(lo[0], lo[1], 'o')
-    plt.text(lo[0] + .01, lo[1], l, horizontalalignment='center', verticalalignment='center')
-
-plt.title('%d ' % V + 'Vehicle routes' if V > 1 else 'Vehicle route')
+    plt.plot(lo[0],lo[1],'o')
+    plt.text(lo[0]+.01,lo[1],l,horizontalalignment='center',verticalalignment='center')
+    
+    
+plt.title('%d '%V + 'Vehicle routes' if V > 1 else 'Vehicle route')
 plt.xlabel('Latitude')
 plt.ylabel('Longitude')
-plt.show()
+plt.show()        
